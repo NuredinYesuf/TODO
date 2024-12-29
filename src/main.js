89bca0cb117886953314737093fd38c8b8e7b126
NuredinYesuf <se.nuredin.yesuf@gmail.com>
2024-12-30 00:25:18 +0300

    main

--- src/main.js
+++ src/main.js
@@ -7,4 +7,6 @@ createApp(App).mount('#app')
 
 
 // identify the main purpose of the folloin 
-// gimort s that are a direct link eto the vue file 
+// gimort s that are a direct link eto the vue file
+// the local server seems to change very tim ethe vite comand os promted 
+ 
