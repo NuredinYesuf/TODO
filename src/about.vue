4f04d9b8bcc75677abdbcd72981a2aa4c7b893ea
NuredinYesuf <se.nuredin.yesuf@gmail.com>
2024-12-29 22:53:24 +0300

    vue-initiatd

---
+++ src/about.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <p>
+            Welcome to your Vue.js app!
+        </p>
+        <button @click="sayHello">Click me</button>
+        <p>{{ greeting }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    },
+    data() {
+        return {
+            greeting: ''
+        };
+    },
+    methods: {
+        sayHello() {
+            this.greeting = 'Hello from HelloWorld component!';
+        }
+    }
+};
+</script>
+
+<style scoped>
+.hello {
+    text-align: center;
+    margin-top: 40px;
+}
+button {
+    margin-top: 20px;
+    padding: 10px 20px;
+    font-size: 16px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,3 +3,7 @@ import App from './App.vue'
 import './main.css'
 
 createApp(App).mount('#app')
+
+
+// identify the main purpose of the folloin 
+// gimort s that are a direct link eto the vue file 
